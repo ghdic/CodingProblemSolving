3429b1690de1b92ffe63a9ded37f02d6bc6e8d7f
ghdic77 <ghdic77@gmail.com>
2018-01-03 19:21:00 +0900

    Success

---
+++ Baekjoon_Problem_Solving/2742.c
@@ -0,0 +1,43 @@
+/*
+https://www.acmicpc.net/problem/2742
+
+&Title
+기찍 N
+
+&Question
+자연수 N이 주어졌을 때, N부터 1까지 한 줄에 하나씩 출력하는 프로그램을 작성하시오.
+
+&Input
+첫째 줄에 100,000보다 작거나 같은 자연수 N이 주어진다.
+
+&Output
+첫째 줄부터 N번째 줄 까지 차례대로 출력한다.
+
+&Example
+-input
+5
+-output
+5
+4
+3
+2
+1
+
+
+*/
+
+/*
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	int N;
+
+	scanf("%d", &N);
+
+	for (int i = N; i > 0; i--)
+		printf("%d\n", i);
+
+	return 0;
+}
+*/
