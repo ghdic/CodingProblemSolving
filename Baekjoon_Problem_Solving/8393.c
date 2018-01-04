5b5f77da02c37595acdaf0f4b9f14895389daad5
ghdic77 <ghdic77@gmail.com>
2018-01-05 02:48:34 +0900

    Success

---
+++ Baekjoon_Problem_Solving/8393.c
@@ -0,0 +1,39 @@
+/*
+https://www.acmicpc.net/problem/8393
+
+&Title
+합
+
+&Question
+n이 주어졌을 때, 1부터 n까지 합을 구하는 프로그램을 작성하시오.
+
+&Input
+첫째 줄에 n (1 ≤ n ≤ 10,000)이 주어진다.
+
+&Output
+1부터 n까지 합을 출력한다.
+
+&Example
+-input
+3
+-output
+6
+
+*/
+
+/*
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+
+	int N;
+
+	scanf("%d", &N);
+
+	printf("%d\n", N*(N + 1) / 2);
+
+	return 0;
+}
+
+*/
