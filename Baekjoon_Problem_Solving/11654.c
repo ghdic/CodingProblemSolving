2b5d24ac68ed707cfcd0a9cc78cf2e088a646f36
ghdic77 <ghdic77@gmail.com>
2018-01-10 15:55:19 +0900

    Success

---
+++ Baekjoon_Problem_Solving/11654.c
@@ -0,0 +1,37 @@
+/*
+https://www.acmicpc.net/problem/
+
+&Title
+아스키 코드
+
+&Question
+알파벳 소문자, 대문자, 숫자 0-9중 하나가 주어졌을 때, 주어진 글자의 아스키 코드값을 출력하는 프로그램을 작성하시오.
+
+&Input
+알파벳 소문자, 대문자 숫자 0-9 중 하나가 첫째 줄에 주어진다.
+
+&Output
+입력으로 주어진 글자의 아스키 코드 값을 출력한다.
+
+&Example
+-input
+A
+-output
+65
+
+*/
+
+/*
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	char N;
+
+	scanf("%c", &N);
+
+	printf("%d\n", N);
+
+	return 0;
+}
+*/
