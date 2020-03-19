6dbc870b2ee51824950e07f2f386a1a941e4c76a
ghdic <ghdic@naver.com>
2020-03-19 17:29:54 +0900

    s, e에 cap 2씩 주면 왕복 하는구만..

--- BaekjoonProblemSolving/BaekjoonProblemSolving/2311.cpp
+++ BaekjoonProblemSolving/BaekjoonProblemSolving/2311.cpp
@@ -45,4 +45,104 @@ N, 1 ≤ B ≤ N, 1 ≤ C
 
 */
 
+/*
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <utility>
+using namespace std;
+typedef pair<int, int> pii;
 
+struct MinCostMaxFlow {
+	struct Edge {
+		int to, c, f, w;
+		Edge* r;
+		Edge(int to = -1, int c = 0, int w = 0) :to(to), c(c), f(0), w(w), r(nullptr) {}
+		int spare() {
+			return c - f;
+		}
+		void add(int f1) {
+			f += f1;
+			r->f -= f1;
+		}
+	};
+	vector<vector<Edge*>> adj;
+	int SZ;
+
+	MinCostMaxFlow(int SZ) :SZ(SZ) {
+		adj.resize(SZ);
+	}
+
+	void addEdge(int u, int v, int c, int w) {
+		Edge* uv = new Edge(v, c, w);
+		Edge* vu = new Edge(u, 0, -w);
+		uv->r = vu;
+		vu->r = uv;
+		adj[u].push_back(uv);
+		adj[v].push_back(vu);
+	}
+
+	pii mcmf(int S, int E) {
+		int mc = 0, mf = 0;
+
+		while (1) {
+			vector<int> prv(SZ, -1), dist(SZ, 0x3f3f3f3f);
+			vector<Edge*> path(SZ);
+			queue<int> q;
+			vector<bool> inQ(SZ);
+
+			q.push(S);
+			dist[S] = 0;
+			while (q.size()) {
+				int cur = q.front(); q.pop();
+				inQ[cur] = false;
+
+				for (Edge* e : adj[cur]) {
+					int next = e->to;
+					if (e->spare() > 0 && dist[next] > dist[cur] + e->w) {
+						dist[next] = dist[cur] + e->w;
+						prv[next] = cur;
+						path[next] = e;
+						if (!inQ[next]) {
+							inQ[next] = true;
+							q.push(next);
+						}
+					}
+				}
+			}
+			if (prv[E] == -1)break;
+
+			int flow = 0x3f3f3f3f;
+			for (int i = E; i != S; i = prv[i])
+				flow = min(flow, path[i]->spare());
+			for (int i = E; i != S; i = prv[i]) {
+				mc += flow * path[i]->w;
+				path[i]->add(flow);
+			}
+			mf += flow;
+		}
+		return { mc,mf };
+	}
+};
+
+int n, m;
+int s = 0, e = 1001;
+MinCostMaxFlow mcmf(1002);
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cin >> n >> m;
+	int a, b, c;
+	while (m--) {
+		cin >> a >> b >> c;
+		mcmf.addEdge(a, b, 1, c);
+		mcmf.addEdge(b, a, 1, c);
+	}
+	mcmf.addEdge(s, 1, 2, 0);
+	mcmf.addEdge(n, e, 2, 0);
+	cout << mcmf.mcmf(s, e).first << endl;
+	return 0;
+}
+*/
